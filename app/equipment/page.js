0bbb503c6a7cf20983e3b1a426ad5b4580d762bc
jacobforever91 <132018561+jacobforever91@users.noreply.github.com>
2026-09-23 23:13:12 -0400

    Add equipment browser page

---
+++ app/equipment/page.js
@@ -0,0 +1,10 @@
+import EquipmentBrowser from "./EquipmentBrowser";
+
+export const metadata = {
+  title: "Class Equipment | AION 2 VISION",
+  description: "Browse AION 2 Global weapons, armor and accessories by class.",
+};
+
+export default function EquipmentPage() {
+  return <EquipmentBrowser />;
+}
