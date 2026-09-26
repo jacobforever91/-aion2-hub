37b433723dbfa83386d075f9eb7336378aca0371
jacobforever91 <132018561+jacobforever91@users.noreply.github.com>
2026-09-26 10:04:15 -0400

    Rebrand page metadata to DAEVEXUS

--- app/wings/page.js
+++ app/wings/page.js
@@ -1,7 +1,7 @@
 import ProgressionCatalog from "../progression/ProgressionCatalog";
 
 export const metadata = {
-  title: "Wings | AION 2 VISION",
+  title: "Wings | DAEVEXUS",
   description: "Browse AION 2 wings, faction, grade, and documented enchantment data.",
 };
 
