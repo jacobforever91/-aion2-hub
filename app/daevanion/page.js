3c9ff5538c547eaba6a502d072010699227f9b11
jacobforever91 <132018561+jacobforever91@users.noreply.github.com>
2026-09-26 07:05:25 -0400

    Add Daevanion encyclopedia route

---
+++ app/daevanion/page.js
@@ -0,0 +1,10 @@
+import DaevanionCatalog from "./DaevanionCatalog";
+
+export const metadata={
+  title:"Daevanion | AION 2 VISION",
+  description:"Explore AION 2 Daevanion boards, nodes, skill upgrades and progression by class."
+};
+
+export default function DaevanionPage(){
+  return <DaevanionCatalog/>;
+}
